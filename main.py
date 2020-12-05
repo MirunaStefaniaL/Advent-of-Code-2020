38fcf745b481fda09d1f5c0cc9398331198b3978
MirunaStefaniaL <40474250+MirunaStefaniaL@users.noreply.github.com>
2020-12-05 09:38:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,82 @@ def p2():
     return ct
 
 print(p2())
+
+
+
+def p3():
+    f = open("f1.txt")
+    a = f.readlines()
+    n = len(a) #nr linii
+    linie = a[0]
+    m = len(linie) - 1#nr coloane
+    matrix = [['0' for x in range(m)] for y in range(n)]
+    for i in range(n):
+        linie = a[i]
+        for j in range(m):
+            x = linie[j]
+            matrix[i][j] = x
+    line = 0
+    column = 0
+    ct1 = 0
+
+    while line <= n - 1:
+        if (matrix[line][column] == '#'):
+            ct1 += 1
+        line += 1
+        column += 1
+        if column >= m:
+            column = column % m
+
+    line = 0
+    column = 0
+    ct2 = 0
+
+    while line <= n - 1:
+        if (matrix[line][column] == '#'):
+            ct2 += 1
+        line += 1
+        column += 3
+        if column >= m:
+            column = column % m
+
+    line = 0
+    column = 0
+    ct3 = 0
+
+    while line <= n - 1:
+        if (matrix[line][column] == '#'):
+            ct3 += 1
+        line += 1
+        column += 5
+        if column >= m:
+            column = column % m
+
+    line = 0
+    column = 0
+    ct4 = 0
+
+    while line <= n - 1:
+        if (matrix[line][column] == '#'):
+            ct4 += 1
+        line += 1
+        column += 7
+        if column >= m:
+            column = column % m
+
+    line = 0
+    column = 0
+    ct5 = 0
+
+    while line <= n - 1:
+        if (matrix[line][column] == '#'):
+            ct5 += 1
+        line += 2
+        column += 1
+        if column >= m:
+            column = column % m
+
+    ct = ct1 * ct2 * ct3 * ct4 * ct5
+    print(ct)
+
+p3()
